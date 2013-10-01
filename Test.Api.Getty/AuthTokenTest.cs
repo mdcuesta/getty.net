5905af2af0eafbc9c2cf396349799f605b6c3dfd
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2013-10-01 22:17:27 +0800

    added AuthTokenTest.cs

---
+++ Test.Api.Getty/AuthTokenTest.cs
@@ -0,0 +1,17 @@
+﻿using Api.Getty;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Api.Getty
+{
+    [TestClass]
+    public class AuthTokenTest
+    {
+        [TestMethod]
+        public void TokenNotExpiredTest()
+        {
+            var authToken = new AuthToken();
+            authToken.TokenDurationMinutes = 30;
+            Assert.IsTrue(!authToken.IsExpired);
+        }
+    }
+}
